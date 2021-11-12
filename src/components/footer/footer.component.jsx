5a4031a8db46f717172b013d488eb4ef349cbe1f
Alexandre Soares <alexandre.desousasoares@gmail.com>
2021-11-12 11:16:09 +0200

    adding header and footer component

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./footer.styles.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <p>This app was created with api.video.</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./header.styles.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>Upload a video to api.video</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
