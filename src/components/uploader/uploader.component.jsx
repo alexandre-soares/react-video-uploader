4f9a1b072be8d105a8c3f57883447cb407e2ed5f
Alexandre Soares <alexandre.desousasoares@gmail.com>
2021-11-12 14:30:45 +0200

    adding progress bar

--- src/components/uploader/uploader.component.jsx
+++ src/components/uploader/uploader.component.jsx
@@ -7,32 +7,45 @@ class Uploader extends Component {
   constructor() {
     super();
 
+    // Defining variables in state
     this.state = {
       videoId: "",
       playerUrl: "",
+      totalPercentComplete: 0,
+      showProgressBar: false,
+      apiKey: "to6IC1ehLEWyGyLbpa9HqOrW",
     };
   }
 
+  // upload file function (event is here to get the video)
   uploadFile = (event) => {
-    console.log("upload commencing");
+    // start uploading
     const uploader = new VideoUploader({
       file: event.target.files[0],
-      uploadToken: "to6IC1ehLEWyGyLbpa9HqOrW",
+      uploadToken: this.state.apiKey,
       chunkSize: 1024 * 1024 * 10, // 10MB
       retries: 10,
     });
+
+    // on progress to track uploading process (progress bar)
     uploader.onProgress((event) => {
-      var totalPercentComplete = Math.round(
-        (event.uploadedBytes / event.totalBytes) * 100
-      );
-      document.getElementById("chunk-information").innerHTML =
-        "Total uploaded: " + totalPercentComplete + "%";
+      // show progress bar
+      this.setState({
+        showProgressBar: true,
+      });
+
+      // define percentage uploading progress
+      this.setState({
+        totalPercentComplete: Math.round(
+          (event.uploadedBytes / event.totalBytes) * 100
+        ),
+      });
     });
 
+    // when upload is finished, show video alert with the link to see the video
     uploader.upload().then((video) => {
       this.playerUrl = video.assets.player;
-      console.log("all uploaded! Watch here: ", this.playerUrl);
-
+      // creating video success alert
       document.getElementById("video-information").innerHTML =
         "Everyting is uploaded! You can watch the video <a href='" +
         this.playerUrl +
@@ -53,13 +66,15 @@ class Uploader extends Component {
           />
           <br />
 
-          <div class="mb-3">
+          {/* Form */}
+
+          <div className="form">
             <label
               htmlFor="formFile"
               id="video-file-label"
               className="form-label"
             >
-              Click here to upload a video
+              Click on the input below to upload a video
             </label>
             <input
               className="form-control"
@@ -70,9 +85,25 @@ class Uploader extends Component {
           </div>
 
           <br />
-          <br />
 
-          <div id="chunk-information" className="alert alert-success"></div>
+          {/* Progress bar div */}
+
+          {this.state.showProgressBar ? (
+            <div className="progress mb-6">
+              <div
+                className="progress-bar"
+                role="progressbar"
+                style={{ width: this.state.totalPercentComplete + "%" }}
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                {this.state.totalPercentComplete}%
+              </div>
+            </div>
+          ) : null}
+
+          {/* Video information alert div */}
+
           <div id="video-information" className="alert alert-success"></div>
         </div>
       </div>
